8561b57b4874ee4a45d8d8bc24dea9cdda588dec
KarolineH <karoline@heiwolt.de>
2020-07-28 14:50:45 +0100

    Added pointnet++ implementation

---
+++ data_handling.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow import keras
+import open3d as o3d
+
+pcd = o3d.io.read_point_cloud("./synth_data/split_clouds/1_1.ply")
+import pdb; pdb.set_trace()
--- sample_points.sh
+++ sample_points.sh
@@ -3,7 +3,7 @@ DENSITYVAR="10000"
 
 for i in {1..6001}
 do
-  cloudcompare.CloudCompare -AUTO_SAVE OFF \
+  cloudcompare.CloudCompare -SILENT -AUTO_SAVE OFF \
    -O synth_data/split_by_organ_obj/${i}_1.obj -SAMPLE_MESH DENSITY $DENSITYVAR -C_EXPORT_FMT PLY -SAVE_CLOUDS FILE "synth_data/split_clouds/${i}_1.ply" -CLEAR_CLOUDS -CLEAR_MESHES \
    -O synth_data/split_by_organ_obj/${i}_2.obj -SAMPLE_MESH DENSITY $DENSITYVAR -C_EXPORT_FMT PLY -SAVE_CLOUDS FILE "synth_data/split_clouds/${i}_2.ply" -CLEAR_CLOUDS -CLEAR_MESHES \
    -O synth_data/split_by_organ_obj/${i}_3.obj -SAMPLE_MESH DENSITY $DENSITYVAR -C_EXPORT_FMT PLY -SAVE_CLOUDS FILE "synth_data/split_clouds/${i}_3.ply" -CLEAR_CLOUDS -CLEAR_MESHES
